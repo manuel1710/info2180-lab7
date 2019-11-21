d0b11555afd72ee63abff551cd622b323fcfba2a
Ubuntu <ubuntu@ip-172-31-88-135.ec2.internal>
2019-11-21 03:58:25 +0000

    Updated Files

---
+++ world.js
@@ -0,0 +1,26 @@
+/*global $*/
+$(document).ready(function(){
+    
+    $("#lookup").click(function(){
+        var allVal;
+        
+        if($("#all").is(':checked'))
+        {
+            allVal = 'true';
+        }
+        else
+        {
+            allVal='false';
+        }
+        
+    
+       
+       $.get('world.php',{country:$("#country").val() , all:allVal},function(data){
+            $('#result').html(data);
+        });
+        
+    });
+    
+    
+    
+})
---
+++ world.php
@@ -0,0 +1,31 @@
+<?php
+$host = getenv('IP');
+$username = 'manuel17';
+$password = 'Bushmaster@17';
+$dbname = 'world';
+
+$country= $_GET['country'];//Get country to search for
+$all=$_GET['all'];
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+if(isset($all) == true && isset($country) == true)
+{
+    if($all == 'true')
+    {
+        $stmt = $conn->query('SELECT * FROM countries');
+    }
+    else
+    {
+        $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    }
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    echo '<ul>';
+    foreach ($results as $row) {
+      echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
+    }
+    echo '</ul>';
+}
+else
+{
+    echo ' ';
+}
